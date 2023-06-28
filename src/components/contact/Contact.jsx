a0926eac5ff7c95d82603b9caa836453590f6d26
Vishal Chauhan <vishalchauhan@Vishals-MacBook-Air.local>
2023-06-28 16:57:36 +0530

    modified contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import './contact.scss';
+import React from "react";
+import "./contact.scss";
 
-import {FaTwitterSquare,FaPinterestSquare,FaInstagramSquare,FaFacebookSquare} from 'react-icons/fa'
-import { notify_success, notify_warning } from '../../utils/notifications';
+import {
+  FaGithubSquare,
+  FaLinkedin,
+  FaLink,
+  FaFacebookSquare,
+} from "react-icons/fa";
+import { notify_success, notify_warning } from "../../utils/notifications";
 
 const Contact = () => {
-
   function isValidEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
-  };
+  }
 
   const handleSubmit = (event) => {
-    if(event.keyCode === 13)
-       { if(isValidEmail(event.target.value))
-            {notify_success('Thanks! we will get back to you..');
-            event.target.value='';
-            }
-          else
-            notify_warning('Not a valid email..');
-       }
-
+    if (event.keyCode === 13) {
+      if (isValidEmail(event.target.value)) {
+        notify_success("Thanks! we will get back to you..");
+        event.target.value = "";
+      } else notify_warning("Not a valid email..");
+    }
   };
 
-
   return (
-    <div className='contact_container'>
-        <div className="contact_info">
-            <p>Be in touch with us</p>
-            <div className="contact_input">
-                <input type='email' placeholder='Email here..' onKeyDown={(e) => handleSubmit(e)}/>
-            </div>
+    <div className="contact_container">
+      <div className="contact_info">
+        <p>Our newsletter</p>
+        <div className="contact_input">
+          <input
+            type="email"
+            placeholder="Email here.."
+            onKeyDown={(e) => handleSubmit(e)}
+          />
         </div>
-        <div className="contact_icons">
-            <FaTwitterSquare className='contact_icon'/>
-            <FaPinterestSquare className='contact_icon'/>
-            <FaInstagramSquare className='contact_icon' />
-            <FaFacebookSquare className='contact_icon'/>
+      </div>
+      <div className="contact_icons">
+        <p>Be in touch with us</p>
+        <div className="contact_icons_conatiner">
+          <FaGithubSquare className="contact_icon" />
+          <FaLinkedin className="contact_icon" />
+          <FaLink className="contact_icon" />
         </div>
-    
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
